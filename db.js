3102770aee6043287123b9598bc3596d22ef585b
Nate Dean <nate.dean@selectquotesenior.com>
2017-02-13 18:27:01 -0600

    Added client passed sorting params to users api

--- db.js
+++ db.js
@@ -3,13 +3,20 @@ const MongoClient = require('mongodb').MongoClient;
 
 if (!MONGO_URI) throw('MONGO_URI environment variable must be set');
 
-const getUsers = (pageOffset, limit) =>
+const getUsers = (pageOffset, limit, sortField, sortCode) =>
   MongoClient.connect(MONGO_URI)
     .then(db => {
       const collection = db.collection('users');
       const fieldsToRetrieve = { username: 1, gtScore: 1, _created_at: 1, _updated_at: 1 };
 
-      return collection.find({}, fieldsToRetrieve).skip(pageOffset).limit(limit).toArray()
+      console.log(pageOffset, limit, sortField, sortCode);
+
+      return collection
+        .find({}, fieldsToRetrieve)
+        .skip(pageOffset)
+        .limit(limit)
+        .sort([sortField, sortCode])
+        .toArray()
         .then(users => {
           db.close();
           console.log(users.length);
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const db = require('./db');
 app.use(cors());
 
 app.get('/users', (req, res) => {
-  const limit = 5;
+  const limit = 20;
   const page = req.query.page || 1;
   const pageOffset = page <= 1 ? 0 : page * limit;
+  const sortDirection = req.query.sortDirection || 'desc';
+  const sortCode = sortDirection === 'desc' ? -1 : 1;
+  const sortField = req.query.sortField || '_create_at';
 
-  db.getUsers(pageOffset, limit)
+  db.getUsers(pageOffset, limit, sortField, sortCode)
     .then(users => res.send(users))
     .catch(() => res.send('There has been an error connecting to the database'));
 });
@@ -21,5 +24,5 @@ app.get('/user/:id', (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log('Server listening on PORT 3000')
+  console.log('Server listening on PORT 3001')
 });
