f7b919416dc0b2db9afd9c9826898dd467f5e39a
Nate Dean <nate.dean@selectquote.com>
2017-03-15 23:29:02 -0500

    Saving daily denormalized statistics in db for quick lookup

--- getStats.js
+++ getStats.js
@@ -10,8 +10,23 @@ const _aggregateStats = () => {
 
       const projectionOne = {
         $project: {
+          _id: 0,
           totalCorrect: 1,
-          percentCorrect: { $divide: [ '$totalCorrect', { $add: ['$totalCorrect', '$totalIncorrect'] }  ] }
+          totalAttempts: { $add: ['$totalCorrect', '$totalIncorrect'] }
+        }
+      };
+
+      const match = {
+        $match: {
+          totalAttempts: { $ne: null, $gt: 5 }
+        }
+      };
+
+      const projectionTwo = {
+        $project: {
+          totalCorrect: 1,
+          totalAttempts: 1,
+          correctRatio: { $divide: [ '$totalCorrect', '$totalAttempts' ] }
         }
       };
 
@@ -19,34 +34,81 @@ const _aggregateStats = () => {
         $group: {
           _id: null,
           avgTotalCorrect: { $avg: '$totalCorrect'  },
-          avgPercentCorrect: { $avg: '$percentCorrect' }
+          avgTotalAttempts: { $avg: '$totalAttempts' },
+          avgCorrectRatio: { $avg: '$correctRatio' }
         }
       };
 
-      const projectionTwo = {
+      const projectionThree = {
         $project: {
           _id: 0,
           avgTotalCorrect: 1,
-          avgPercentCorrect: 1
+          avgTotalAttempts: 1,
+          avgCorrectRatio: 1
         }
       };
 
-      collection.aggregate([projectionOne, group, projectionTwo], (err, result) => {
+      collection.aggregate([projectionOne, match, projectionTwo, group, projectionThree], (err, res) => {
         db.close();
 
-        return resolve(Object.assign({}, result[0]));
+        return resolve(Object.assign({}, _roundValues(res[0])));
       });
     });
   });
+};
+
+
+const saveDailyStatsToDb = (stats) => {
+  const date = new Date();
+  const dateString = date.toLocaleDateString();
+
+  getCollection('dailyStats').then(({collection, db}) => {
+    const update = Object.assign({}, stats, { dateString, _updated_at: date });
+    collection.findOneAndUpdate(
+        { dateString },
+        { $set: update },
+        { $sort: { date: 1 }, upsert: true, returnNewDocument: true }
+      ).then(res => { db.close(); })
+      .catch(err => { db.close(); });
+  });
+};
+
+const getTableCounts = () => {
+  const usersCountPromise = getCollection('users').then(({ collection, db }) => {
+    return collection.find({}).count().then(res => {
+      db.close();
+      return res;
+    });
+  });
+
+  const questionsCountPromise = getCollection('questions').then(({ collection, db }) => {
+    return collection.find({}).count().then(res => {
+      db.close();
+      return res;
+    });
+  });
+
+  return Promise.all([usersCountPromise, questionsCountPromise])
+    .then(([usersCount, questionsCount]) => {
+      return { usersCount, questionsCount };
+    });
 };
 
 // get amount of users for each individual _created_at Y-m-d in the collection
 //aggregate([{ $group: { _id: { $dateToString: { format: '%Y-%m-%d', date: '$_created_at' } }, count: { $sum: 1 }  }  },{  $sort: { _id: -1 }  } ]);
 
 const _pollStats = () => {
-  _aggregateStats().then((stats) => {
-    console.log('setting stats', JSON.stringify(stats));
-    cachedStats = Object.assign({}, cachedStats, stats);
+  _aggregateStats().then((aggregateStats) => {
+    console.log('setting aggregateStats', JSON.stringify(aggregateStats));
+
+    // save locally for fast retrieval
+    cachedStats = Object.assign({}, cachedStats, aggregateStats);
+
+    getTableCounts().then(tableCounts => {
+      return Object.assign({}, aggregateStats, tableCounts);
+    }).then(allStats => {
+      saveDailyStatsToDb(allStats);
+    });
   });
 };
 
@@ -54,3 +116,10 @@ _pollStats();
 setInterval(_pollStats, 100000);
 
 module.exports = getStats;
+
+function _roundValues(obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[key] = parseFloat(obj[key]).toFixed(2);
+    return acc;
+  }, {});
+}
